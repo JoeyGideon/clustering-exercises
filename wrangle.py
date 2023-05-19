2ab03fe214e39f3495b7f5b9f2fd88dc6fe4baab
Joey Gideon <joey.gideon1@gmail.com>
2023-05-19 16:45:14 -0500

    added def

--- wrangle.py
+++ wrangle.py
@@ -8,21 +8,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 
-
-
-
-def handle_missing_values(df, prop_required_column, prop_required_row):
-    # Drop columns based on the proportion of missing values
-    column_threshold = int(df.shape[0] * prop_required_column)
-    df = df.dropna(axis=1, thresh=column_threshold)
-
-    # Drop rows based on the proportion of missing values
-    row_threshold = int(df.shape[1] * prop_required_row)
-    df = df.dropna(axis=0, thresh=row_threshold)
-
-    return df
-
-
 def get_zillow_data():
     """
     This function connects to the zillow database and retrieves data from the properties_2017 table for
@@ -84,6 +69,9 @@ def handle_missing_values(df, prop_required_column, prop_required_row):
     # Drop rows based on the proportion of missing values
     row_threshold = int(df.shape[1] * prop_required_row)
     df = df.dropna(axis=0, thresh=row_threshold)
+
+    # Fill remaining nulls or NAs with the median value of each column
+    df = df.fillna(df.median())
 
     return df
 
